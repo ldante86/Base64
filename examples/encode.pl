b50d92172133e76fde69a4b05b91c9aa0a651ec4
Luciano Dante Cecere <ldante86@aol.com>
2017-11-04 21:15:34 -0800

    update

--- examples/encode.pl
+++ examples/encode.pl
@@ -13,6 +13,13 @@ use Base64 qw(encode);
 ## Usage: encode.pl < file
 ## or read from standard input
 
+# 0 = wrap off; 1 = wrap on
+$Base64::wrap = 1;
+
+# Set wrap width. Default 80. Set width to 0
+# to disable wrapping.
+$Base64::width = 80;
+
 my $file = '';
 
 foreach my $line (<STDIN>) {
